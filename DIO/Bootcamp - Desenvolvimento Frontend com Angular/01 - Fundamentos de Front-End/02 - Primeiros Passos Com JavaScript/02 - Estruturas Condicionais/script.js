3701b67eb971d3b18be90d55385745089517bf56
MauricioL88 <lisboa.sse@gmail.com>
2024-01-17 20:27:43 -0300

    Atualização JS

---
+++ DIO/Bootcamp - Desenvolvimento Frontend com Angular/01 - Fundamentos de Front-End/02 - Primeiros Passos Com JavaScript/02 - Estruturas Condicionais/script.js
@@ -0,0 +1,11 @@
+// tipos booleanos são representados por "true" ou "false".
+
+const mauricioEstudante = true;
+const mauricioProgramador = false; // ainda!!!
+
+// Operador do resto da divisão é "%"
+
+const numero = 5;
+const numeroPar = numero % 2;
+
+console.log(numeroPar);
